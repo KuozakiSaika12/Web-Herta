8d0b65b35fd917ea38e6a6dade9a37ec693a1099
KuozakiSaika12 <kuozakisaika3112@gmail.com>
2024-11-24 20:52:03 +0700

    Add some new script

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 const currentPath = window.location.pathname.split('/').pop();
 
-document.querySelectorAll('.nav a').forEach(link =>{
+document.querySelectorAll('.main-nav .nav a').forEach(link =>{
     if(link.getAttribute('href').includes(currentPath)){
         link.classList.add('active')
     }else{
