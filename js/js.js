4c5c0897f8bdc0a6f06fa82fa01473c52656b8b1
KuozakiSaika12 <kuozakisaika3112@gmail.com>
2024-11-25 20:42:48 +0700

    Update Footer StickyNav

--- js/js.js
+++ js/js.js
@@ -1,13 +1,4 @@
-// const currentPath = window.location.pathname.split('/').pop();
-
-// document.querySelectorAll('.main-nav .nav a').forEach(link =>{
-//     if(link.getAttribute('href').includes(currentPath)){
-//         link.classList.add('active')
-//     }else{
-//         link.classList.remove('active')
-//     }
-// })
-
+// Active navbar link
 const currentPath = window.location.pathname.split('/').pop(); // ดึงชื่อไฟล์ของหน้า
 document.querySelectorAll('.nav a').forEach(link => {
 const linkPath = link.getAttribute('href'); // ดึงค่า href ของ link
@@ -17,5 +8,19 @@ const linkPath = link.getAttribute('href'); // ดึงค่า href ของ
         link.classList.remove('active'); // ลบ active class ถ้าไม่ตรง
     }
 });
+
+
+// sticky nav 
+
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('.header-area');
+
+    if (window.scrollY > 0) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
+
 
 
