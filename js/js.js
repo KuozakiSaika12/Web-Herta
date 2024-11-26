d70ce91e1c59fd902088712929be41fea428b347
KuozakiSaika12 <kuozakisaika3112@gmail.com>
2024-11-26 19:33:29 +0700

    Update Responesive

--- js/js.js
+++ js/js.js
@@ -1,26 +1,26 @@
 // Active navbar link
-const currentPath = window.location.pathname.split('/').pop(); // ดึงชื่อไฟล์ของหน้า
+const currentPath = window.location.pathname.split('/').pop();                                          // ดึงชื่อไฟล์ของหน้า
 document.querySelectorAll('.nav a').forEach(link => {
-const linkPath = link.getAttribute('href'); // ดึงค่า href ของ link
+const linkPath = link.getAttribute('href');                                                 // ดึงค่า href ของ link
     if (linkPath === currentPath || (currentPath === '' && linkPath === 'index.html')) {
-        link.classList.add('active'); // เพิ่ม active class
+        link.classList.add('active');                                                               // เพิ่ม active class
     } else {
-        link.classList.remove('active'); // ลบ active class ถ้าไม่ตรง
+        link.classList.remove('active');                                                        // ลบ active class ถ้าไม่ตรง
     }
 });
 
 
 // sticky nav 
 
-document.addEventListener('scroll', () => {
-    const header = document.querySelector('.header-area');
+// document.addEventListener('scroll', () => {
+//     const header = document.querySelector('.header-area');
 
-    if (window.scrollY > 0) {
-        header.classList.add('sticky');
-    } else {
-        header.classList.remove('sticky');
-    }
-});
+//     if (window.scrollY > 0) {
+//         header.classList.add('sticky');
+//     } else {
+//         header.classList.remove('sticky');
+//     }
+// });
 
 
 
